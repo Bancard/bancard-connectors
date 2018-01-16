b6d38dfa05ae7441b707c6e43a773e02b1d7b5e4
Bruno Bradach <bruno.bradach@moove-it.com>
2018-01-16 14:03:45 -0300

    [VPOS-69] - Add function to initialize checkout form

--- vpos/checkout/javascript/bancard-checkout.js
+++ vpos/checkout/javascript/bancard-checkout.js
@@ -1,59 +1,67 @@
 ((function bancardCheckout(window) {
   var Settings = {
-    IframeUrl: 'https://desa.infonet.com.py:8085/checkout/new?is_test_client=true&',
+    CheckoutIframeUrl: 'https://desa.infonet.com.py:8085/checkout/new?is_test_client=true&',
+    BancardUrl: 'https://desa.infonet.com.py:8085',
     DivId: ''
   };
 
   var internalMethods = {
     redirect: function redirect(event) {
+      if (event.origin !== Settings.BancardUrl) {
+        return;
+      }
+
       var url = event.data.return_url;
+
       window.location.replace(url);
     },
 
     setListener: function setListener() {
       window.addEventListener('message', internalMethods.redirect);
+    },
+
+    createForm: function createForm(divId, processId, styles, iframeUrl) {
+      var iframeContainer;
+      var iframe;
+      var lastIframeUrlChar;
+
+      Settings.DivId = divId;
+
+      iframeContainer = document.getElementById(divId);
+      iframe = document.createElement('iframe');
+
+      lastIframeUrlChar = iframeUrl.slice(-1);
+
+      if (['&', '?'].indexOf(lastIframeUrlChar) > -1) {
+        iframeUrl += 'process_id=' + processId;
+      } else if (iframeUrl.indexOf('?') > -1) {
+        iframeUrl += '&process_id=' + processId;
+      } else {
+        iframeUrl += '?process_id=' + processId;
+      }
+
+      if (typeof styles !== 'undefined') {
+        iframeUrl += '&styles=' + encodeURIComponent(JSON.stringify(styles));
+      }
+
+      iframe.src = iframeUrl;
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.borderWidth = '0px';
+
+      iframeContainer.innerHTML = '';
+      iframeContainer.appendChild(iframe);
+
+      internalMethods.setListener();
     }
   };
 
   var BancardCheckout = function BancardCheckout() {};
 
-  BancardCheckout.prototype.init = function init(divId, processId, styles) {
-    var iframeUrl;
-    var iframeContainer;
-    var iframe;
-    var lastIframeUrlChar;
-
-    Settings.DivId = divId;
-
-    iframeUrl = Settings.IframeUrl;
-    iframeContainer = document.getElementById(divId);
-    iframe = document.createElement('iframe');
-
-    lastIframeUrlChar = iframeUrl.slice(-1);
-
-    if (['&', '?'].indexOf(lastIframeUrlChar) > -1) {
-      iframeUrl += 'process_id=' + processId;
-    } else if (iframeUrl.indexOf('?') > -1) {
-      iframeUrl += '&process_id=' + processId;
-    } else {
-      iframeUrl += '?process_id=' + processId;
-    }
-
-    if (typeof styles !== 'undefined') {
-      iframeUrl += '&styles=' + encodeURIComponent(JSON.stringify(styles));
-    }
-
-    iframe.src = iframeUrl;
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.borderWidth = '0px'
-
-    iframeContainer.innerHTML = '';
-    iframeContainer.appendChild(iframe);
-
-    internalMethods.setListener();
-  };
-
+  BancardCheckout.prototype.createCheckoutForm =
+    function createCheckoutForm(divId, processId, styles) {
+      internalMethods.createForm(divId, processId, styles, Settings.CheckoutIframeUrl);
+    };
 
   BancardCheckout.prototype.destroy = function destroy() {
     var iframeContainer = document.getElementById(Settings.DivId);
