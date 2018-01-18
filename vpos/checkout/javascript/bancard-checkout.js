1888a0d93b67cf36b413dd58ca51eb55a1baced3
Bruno Bradach <bruno.bradach@moove-it.com>
2018-01-18 14:05:54 -0300

    [VPOS-86] - Refactor suggested by Gian

--- vpos/checkout/javascript/bancard-checkout.js
+++ vpos/checkout/javascript/bancard-checkout.js
@@ -1,9 +1,9 @@
-((function bancardCheckout(window) {
+((function bancard(window) {
   var BancardUrl = 'https://desa.infonet.com.py:8085';
 
   var Settings = {
     CheckoutIframeUrl: BancardUrl + '/checkout/new',
-    DivId: ''
+    DivId: null
   };
 
   var internalMethods = {
@@ -17,7 +17,7 @@
 
       url = internalMethods.addParamToUrl(url, 'status', status);
 
-      window.location.replace(url);
+      location.assign(url);
     },
 
     setListener: function setListener() {
@@ -28,7 +28,7 @@
       var lastUrlChar = url.slice(-1);
       var paramValue = param + '=' + value;
 
-      if (['&', '?'].indexOf(url) > -1) {
+      if (['&', '?'].indexOf(lastUrlChar) > -1) {
         url += paramValue;
       } else if (url.indexOf('?') > -1) {
         url += '&' + paramValue;
@@ -40,12 +40,24 @@
     },
 
     createForm: function createForm(divId, processId, styles, iframeUrl) {
-      var iframeContainer;
-      var iframe;
+      var iframeContainer, iframe;
+
+      if (typeof divId !== 'string' || divId === '') {
+        throw new InvalidParameter('Div id');
+      }
+
+      if (typeof processId !== 'string' || processId === '') {
+        throw new InvalidParameter('Process id');
+      }
 
       Settings.DivId = divId;
 
       iframeContainer = document.getElementById(divId);
+
+      if (!iframeContainer) {
+        throw new DivDoesNotExist(divId);
+      }
+
       iframe = document.createElement('iframe');
 
       iframeUrl = internalMethods.addParamToUrl(iframeUrl, 'process_id', processId);
@@ -64,27 +76,58 @@
       iframeContainer.appendChild(iframe);
 
       internalMethods.setListener();
+    },
+
+    clearElement: function clearElement(element) {
+      while (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
     }
   };
 
-  var BancardCheckout = function BancardCheckout() {};
+  var Bancard = function Bancard() {};
 
-  BancardCheckout.prototype.createCheckoutForm =
+  Bancard.prototype.Checkout = function Checkout() {};
+
+  Bancard.prototype.Checkout.createForm =
     function createCheckoutForm(divId, processId, styles) {
       internalMethods.createForm(divId, processId, styles, Settings.CheckoutIframeUrl);
     };
 
-  BancardCheckout.prototype.destroy = function destroy() {
+  Bancard.prototype.destroy = function destroy() {
     var iframeContainer = document.getElementById(Settings.DivId);
 
     window.removeEventListener('message', internalMethods.redirect);
 
-    while (iframeContainer.firstChild) {
-      iframeContainer.removeChild(iframeContainer.firstChild);
+    if (iframeContainer) {
+      internalMethods.clearElement(iframeContainer);
     }
+
+    Settings.DivId = null;
   };
 
-  if (typeof (window.BancardCheckout) === 'undefined') {
-    window.BancardCheckout = new BancardCheckout();
+  var DivDoesNotExist = function DivDoesNotExist(divId) {
+    this.name = 'DivDoesNotExist';
+    this.message = 'Div with id: ' + divId + ' could not be found.';
+    this.stack = (new Error()).stack;
+  };
+
+  DivDoesNotExist.prototype = new Error();
+
+  var InvalidParameter = function InvalidParameter(parameter) {
+    this.name = 'InvalidParameter';
+    this.message = parameter + ' must be a non empty string.';
+    this.stack = (new Error()).stack;
+  };
+
+  InvalidParameter.prototype = new Error();
+
+  Bancard.prototype.exceptions = {
+    'DivDoesNotExist': DivDoesNotExist,
+    'InvalidParameter': InvalidParameter
+  };
+
+  if (typeof (window.Bancard) === 'undefined') {
+    window.Bancard = new Bancard();
   }
 })(window));
