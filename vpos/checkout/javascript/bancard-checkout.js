288187d0526cd4dabe41bf7bb55f0500c5d5b40f
Bruno Bradach <bruno.bradach@moove-it.com>
2018-01-23 10:36:01 -0300

    Fix arguments order

--- vpos/checkout/javascript/bancard-checkout.js
+++ vpos/checkout/javascript/bancard-checkout.js
@@ -106,7 +106,7 @@
 
   Bancard.prototype.Checkout.createForm =
     function createCheckoutForm(divId, processId, styles) {
-      internalMethods.createForm(divId, processId, styles, Settings.CheckoutIframeUrl);
+      internalMethods.createForm(divId, processId, Settings.CheckoutIframeUrl, styles);
     };
 
   Bancard.prototype.destroy = function destroy() {
