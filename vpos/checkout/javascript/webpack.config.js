c1402f7a35f61444bb41d0b722ed8dd0b7285219
Cristian Dotta <cdotta90@gmail.com>
2018-02-20 08:49:50 -0300

    Improve webpack's production script

--- vpos/checkout/javascript/webpack.config.js
+++ vpos/checkout/javascript/webpack.config.js
@@ -4,13 +4,13 @@ const Webpack = require('webpack');
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
-  devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin([`dist/bancard-checkout-${process.env.npm_package_version}.js`]),
     new Webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env.VPOS_PORTAL': JSON.stringify('https://vpos.infonet.com.py'),
     }),
+    new Webpack.optimize.UglifyJsPlugin(),
   ],
   output: {
     filename: `bancard-checkout-${process.env.npm_package_version}.js`,
