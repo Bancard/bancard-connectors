0c02cd82801e0c6bd06d4d302eb7495b14ec3366
Ignacio Jorge <nacho.jorge94@gmail.com>
2021-02-24 15:40:14 -0300

    [VPOS-359] - Add preauthorizations iframe

--- vpos/checkout/javascript/src/specs/bancard-checkout.test.js
+++ vpos/checkout/javascript/src/specs/bancard-checkout.test.js
@@ -153,6 +153,24 @@ describe('Bancard', () => {
         });
       });
     });
+
+    describe('Preauthorization', () => {
+      beforeEach(() => {
+        instance.Preauthorization.createForm('targetDiv', '1234');
+        window.location.replace = jest.fn();
+      });
+
+      afterEach(() => { instance.destroy(); });
+
+      test('It creates the iframe', () => {
+        expect(document.querySelectorAll('iframe').length).toBe(1);
+      });
+
+      test('Iframe points to correct URL', () => {
+        expect(document.querySelectorAll('iframe')[0].getAttribute('src'))
+          .toBe('https://desa.infonet.com.py:8085/checkout/preauthorization/new?process_id=1234');
+      });
+    });
   });
 
   describe('When the div does not exist', () => {
